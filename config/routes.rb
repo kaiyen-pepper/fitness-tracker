132bad7ad05bf6087dee112595a9cfec6ce5587a
Kayen <kaitlincyen@gmail.com>
2025-05-09 20:22:47 -0700

    Add REST routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,17 @@ Rails.application.routes.draw do
 
   # This means when someone goes to /users, the app shows the index action in the UsersController
   get "/users", to: "users#index"
+  # RESTful routes. These are standard ways to handle creating, reading, updating, and deleting (CRUD) things in your app
+  resources :users
+  
+  # to make a user profile page
+  get "/profile", to: "users#profile"
+
+  # This lets you reach articles at /admin/articles, keeping things tidy
+  namespace :admin do
+    resources :articles, :comments
+  end
+  
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
