4d2ce8a9a7c9b8fda865257f99f226789e8cb326
Kayen <kaitlincyen@gmail.com>
2025-05-12 22:19:58 -0700

    Added logout function
    Dashboard welcomes user with email

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,12 @@ class HomeController < ApplicationController
   # before_action :authenticate_user!
   def home 
     # if logged in already go straight to dashboard -- can't go back home 
-    redirect_to dashboard_path if authenicated?
+    redirect_to dashboard_path if authenticated?
   end
 
   def dashboard
     # if not logged in only go to login page
-    redirect_to new_session_path unless authenicated?
+    redirect_to new_session_path unless authenticated?
   end
 
   def index
