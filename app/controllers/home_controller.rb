976a73beda26b84ac09607541427d6dacf71befa
Kayen <kaitlincyen@gmail.com>
2025-05-12 22:12:40 -0700

    Added login authentication
    Created user instance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
 class HomeController < ApplicationController
+  allow_unauthenticated_access only: :home
   # before_action :authenticate_user!
   def home 
+    # if logged in already go straight to dashboard -- can't go back home 
+    redirect_to dashboard_path if authenicated?
   end
 
   def dashboard
+    # if not logged in only go to login page
+    redirect_to new_session_path unless authenicated?
   end
 
   def index
